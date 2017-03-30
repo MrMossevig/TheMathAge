e5be5c07a6ef13e31c2632401523830dac31b82c
Alf Petter Mossevig <taperkatt@gmail.com>
2017-03-30 15:37:54 +0200

    Updated to absolute path so they can be run from anywahere

--- run_helpers.py
+++ run_helpers.py
@@ -8,18 +8,18 @@ import xml.etree.ElementTree as ET
 
 
 def list_factions():            
-    ls = os.listdir('The-9th-Age') 
+    ls = os.listdir(os.path.join(os.path.dirname(__file__), 'The-9th-Age'))
     #remove .cat extension
     newlist = []
     
     newlist.append([L[0:-4] for L in ls]) 
-    return newlist
+    return newlist[0]
     
 def list_units(filename=None):
     #filename without extension
     assert filename is not None
     cwd = os.getcwd()    
-    os.chdir('The-9th-Age')
+    os.chdir(os.path.join(os.path.dirname(__file__), 'The-9th-Age'))
     
     tree  = ET.parse(filename+'.cat')
     root = tree.getroot()
